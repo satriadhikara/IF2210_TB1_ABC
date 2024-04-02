f8ee81ed4604cbd981f3d7bb5f0ba7fa2f1d9158
satriadhikara <satriadhikara@gmail.com>
2024-04-02 16:03:06 +0700

    feat: add Walikota.cpp

---
+++ src/classes/Walikota.cpp
@@ -0,0 +1,25 @@
+#include "Walikota.hpp"
+
+using namespace std;
+
+Walikota::Walikota() : Pemain() {}
+
+Walikota::Walikota(int uang, int bb) : Pemain(uang, bb) {}
+
+Walikota::~Walikota() {}
+
+void Walikota::pungutPajak(vector<Petani> &petani, vector<Peternak> &peternak)
+{
+}
+
+void Walikota::bangunBangunan()
+{
+}
+
+void Walikota::tambahAkunPetani(vector<Petani> &petani)
+{
+}
+
+void Walikota::tambahAkunPeternak(vector<Peternak> &peternak)
+{
+}
