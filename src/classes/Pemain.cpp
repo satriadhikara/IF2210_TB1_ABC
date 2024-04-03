7be19d0b678343fcf015e1a3ce464412a63db1dd
JonathanSaragih <jonathan.srgh@gmail.com>
2024-04-03 16:08:03 +0700

    feat: add pemain dan hewan

--- src/classes/Pemain.cpp
+++ src/classes/Pemain.cpp
@@ -1,1 +1,29 @@
 #include "Pemain.hpp"
+#include <iostream>
+
+Pemain::Pemain() : uang(50), bb(40) {}
+
+Pemain::Pemain(int uang, int bb) : uang(uang), bb(bb) {}
+
+Pemain::~Pemain() {}
+
+void Pemain::showPenyimpanan() {
+    std::cout << "Matriks penyimpanan : " << std::endl;
+    //ini masih bingung jujurly
+}
+
+void Pemain::setUang(int uang) {
+    this->uang = uang;
+}
+
+int Pemain::getUang() {
+    return uang;
+}
+
+void Pemain::setBB(int bb) {
+    this->bb = bb;
+}
+
+int Pemain::getBB() {
+    return bb;
+}
